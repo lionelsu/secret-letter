b13f16dcb86c127ec719514b1c344a55175164fe
lionelsu <lionel_suicune@hotmail.com>
2023-02-27 12:56:42 -0400

    req: added functions to add span to text and validate if input is empty

---
+++ script.js
@@ -0,0 +1,38 @@
+const letterInput = document.querySelector('#carta-texto');
+const letterElement = document.querySelector('#carta-gerada');
+const btnGenLetter = document.querySelector('#criar-carta');
+
+// Função de validação, retorna uma mensagem e não faz nada caso o campo de texto esteja vazio.
+function validateInput() {
+  const inputText = letterInput.value.trim();
+  if (!inputText) {
+    console.log('Por favor, insira algum texto.');
+    return false;
+  }
+  return true;
+}
+
+// Função para formatação da carta, adiciona as palavras do input em um array de spans
+function formatLetter() {
+  const words = letterInput.value.split(' ');
+  const formattedWords = [];
+  for (let index = 0; index < words.length; index += 1) {
+    const word = words[index].trim();
+    if (word) {
+      formattedWords.push(`<span>${word}</span>`);
+    }
+  }
+  const formattedText = formattedWords.join(' ');
+
+  return formattedText;
+}
+
+// Função com o único papel de injetar o texto formatado no paragrafo da carta misteriosa, caso o input esteja com algum valor
+function generateLetter() {
+  if (!validateInput()) {
+    return;
+  }
+  const formattedText = formatLetter();
+  letterElement.innerHTML = formattedText;
+}
+btnGenLetter.addEventListener('click', generateLetter);
