a52b7bd9eaa1336dd91e976509cdb1685ec771e6
lionelsu <lionel_suicune@hotmail.com>
2023-02-27 19:08:18 -0400

    req: added function to inject span class to each span in the letter

--- script.js
+++ script.js
@@ -2,6 +2,38 @@ const letterInput = document.querySelector('#carta-texto');
 const letterElement = document.querySelector('#carta-gerada');
 const btnGenLetter = document.querySelector('#criar-carta');
 
+// Grupos de classes para serem usados nos SPAN
+const classes = [
+  'newspaper',
+  'magazine1',
+  'magazine2',
+  'medium',
+  'big',
+  'reallybig',
+  'rotateleft',
+  'rotateright',
+  'skewleft',
+  'skewright',
+];
+
+// Const para definir propriedades para serem passadas ao style.
+const transforms = {
+  rotateleft: 'matrix(0.996195, -0.0871557, 0.0871557, 0.996195, 0, 0)',
+  rotateright: 'matrix(0.996195, 0.0871557, -0.0871557, 0.996195, 0, 0)',
+  skewleft: 'matrix(1, 0, 0.176327, 1, 0, 0)',
+  skewright: 'matrix(1, 0, -0.176327, 1, 0, 0)',
+};
+
+// Função responsável por popular estas propriedades no html que será usado como var no css
+// Tenho forçado um pouco o uso do destructuring
+function setStyle() {
+  const { style } = document.documentElement;
+  Object.entries(transforms).forEach(([prop, value]) => {
+    style.setProperty(`--${prop}`, value);
+  });
+}
+setStyle();
+
 // Função de validação, retorna uma mensagem e não faz nada caso o campo de texto esteja vazio.
 function validateInput() {
   const inputText = letterInput.value.trim();
@@ -28,6 +60,13 @@ function formatLetter() {
   return formattedText;
 }
 
+// Função para distribuir as classes dentro dos SPANS
+function addRandomClasses(element, classList) {
+  const randomIndexClass = Math.floor(Math.random() * classList.length);
+  const randomClass = classList[randomIndexClass];
+  element.classList.add(randomClass);
+}
+
 // Função com o único papel de injetar o texto formatado no paragrafo da carta misteriosa, caso o input esteja com algum valor
 function generateLetter() {
   if (!validateInput()) {
@@ -35,5 +74,8 @@ function generateLetter() {
   }
   const formattedText = formatLetter();
   letterElement.innerHTML = formattedText;
+
+  const spans = letterElement.querySelectorAll('span');
+  spans.forEach((span) => addRandomClasses(span, classes));
 }
 btnGenLetter.addEventListener('click', generateLetter);
